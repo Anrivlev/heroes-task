d13abd1252275acd66bc047a21b766bc848c0ec0
Anrivlev <warcraft1921@gmail.com>
2025-01-18 14:53:49 +0300

    built project

--- src/programs/GeneratePresetImpl.java
+++ src/programs/GeneratePresetImpl.java
@@ -7,10 +7,64 @@ import com.battle.heroes.army.programs.GeneratePreset;
 import java.util.*;
 
 public class GeneratePresetImpl implements GeneratePreset {
+    private static final int MAX_UNIT_PER_TYPE = 11;
 
+    private static final int ROWS = 3;
+
+    private static final int UNITS_IN_A_ROW = 21;
+
+    // Итоговая сложность: O(n * logn), где (O * logn) - сортировка, O(n) - выбор.
     @Override
     public Army generate(List<Unit> unitList, int maxPoints) {
-        // Ваше решение
-        return null;
+        unitList.sort(Comparator.comparingDouble(unit ->
+                -((double) (unit.getBaseAttack() + unit.getHealth()) / unit.getCost())
+        ));
+
+        int computerArmyPoints = 0;
+        List<Unit> generatedUnits = new ArrayList<>();
+
+        for (Unit unit : unitList) {
+            int numberOfUnits = getNumberOfUnits(unit, maxPoints, computerArmyPoints);
+            replenishArmy(unit, numberOfUnits, generatedUnits);
+            computerArmyPoints += numberOfUnits * unit.getCost();
+        }
+
+        setPositionsToUnits(generatedUnits);
+
+        Army computerArmy = new Army(generatedUnits);
+        computerArmy.setPoints(computerArmyPoints);
+
+        return computerArmy;
+    }
+
+    private void setPositionsToUnits(List<Unit> generatedUnits) {
+        Set<String> occupiedPositions = new HashSet<>();
+        Random random = new Random();
+
+        for (Unit unit : generatedUnits) {
+            int xCoordinate, yCoordinate;
+            do {
+                xCoordinate = random.nextInt(ROWS);
+                yCoordinate = random.nextInt(UNITS_IN_A_ROW);
+            } while (occupiedPositions.contains(xCoordinate + ":" + yCoordinate));
+
+            occupiedPositions.add(xCoordinate + ":" + yCoordinate);
+            unit.setxCoordinate(xCoordinate);
+            unit.setyCoordinate(yCoordinate);
+        }
+    }
+
+    private int getNumberOfUnits(Unit unit, int maxPoints, int currentPoints) {
+        return Math.min(MAX_UNIT_PER_TYPE, (maxPoints - currentPoints) / unit.getCost());
+    }
+
+    private void replenishArmy(Unit unit, int numberOfUnits, List<Unit> generatedUnits) {
+        for (int i = 0; i < numberOfUnits; i++) {
+            Unit newUnit = new Unit(unit.getUnitType() + " " + i, unit.getUnitType(), unit.getHealth(),
+                    unit.getBaseAttack(), unit.getCost(), unit.getAttackType(),
+                    unit.getAttackBonuses(), unit.getDefenceBonuses(), 0, 0);
+
+            generatedUnits.add(newUnit);
+        }
     }
 }
--- src/programs/SimulateBattleImpl.java
+++ src/programs/SimulateBattleImpl.java
@@ -7,8 +7,33 @@ import com.battle.heroes.army.programs.SimulateBattle;
 public class SimulateBattleImpl implements SimulateBattle {
     private PrintBattleLog printBattleLog; // Позволяет логировать. Использовать после каждой атаки юнита
 
+    // Сложность:  O(n * m), где
+    // n - количество юнитов в армии игрока,
+    // m - количество юнитов в армии компьютера
     @Override
     public void simulate(Army playerArmy, Army computerArmy) throws InterruptedException {
-        // Ваше решение
+        Set<Unit> playerUnits = new HashSet<>(playerArmy.getUnits());
+        Set<Unit> computerUnits = new HashSet<>(computerArmy.getUnits());
+
+        while (!playerUnits.isEmpty() && !computerUnits.isEmpty()) {
+            attackWithAnArmy(playerUnits);
+            attackWithAnArmy(computerUnits);
+        }
+    }
+
+    private void attackWithAnArmy(Set<Unit> attackingArmy) throws InterruptedException {
+        Iterator<Unit> iterator = attackingArmy.iterator();
+        while (iterator.hasNext()) {
+            Unit attackingUnit = iterator.next();
+            if (!attackingUnit.isAlive()) {
+                iterator.remove();
+                continue;
+            }
+
+            Unit targetUnit = attackingUnit.getProgram().attack();
+            if (targetUnit != null) {
+                printBattleLog.printBattleLog(attackingUnit, targetUnit);
+            }
+        }
     }
 }
